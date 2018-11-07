abd879996ba4a890019307c43261bcdb07ab135c
David Ochoa <david.ochoa@ad.infosys.com>
2018-11-07 10:39:47 -0500

    changed port env

--- server.js
+++ server.js
@@ -6,4 +6,6 @@ var serveStatic = require('serve-static')
 var app = express()
 
 app.use(serveStatic(path.join(__dirname, 'public')))
-app.listen(8080)
+app.listen(process.env.PORT || 8080, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
